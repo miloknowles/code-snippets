015123f52789967bc8a698d6ba7139eb37c025ba
Milo Knowles <miloknowles97@gmail.com>
2024-02-14 12:45:09 -0500

    Path utils

---
+++ snippets/paths.py
@@ -0,0 +1,26 @@
+import os
+
+
+def top_folder(rel='') -> str:
+  """Returns the path to the top of the repository.
+  
+  Notes
+  -----
+  Assumes that this script is one directory down from the top of the repository.
+  """
+  return os.path.join(os.path.abspath(os.path.join(os.path.realpath(__file__), "../../")), rel)
+
+
+def data_folder(rel='') -> str:
+  """Returns a path relative to the `data` folder."""
+  return os.path.join(top_folder('data'), rel)
+
+
+def models_folder(rel='') -> str:
+  """Returns a path relative to the `models` folder."""
+  return os.path.join(top_folder('models'), rel)
+
+
+def tests_folder(rel='') -> str:
+  """Returns a path relative to the `tests` folder."""
+  return os.path.join(top_folder('tests'), rel)
---
+++ tests/test_paths.py
@@ -0,0 +1,13 @@
+import os
+
+from snippets.paths import top_folder, tests_folder
+
+
+def test_paths():
+  print(top_folder())
+  assert(os.path.exists(top_folder()))
+  assert(os.path.exists(top_folder("README.md")))
+
+  print(tests_folder())
+  assert(os.path.exists(tests_folder()))
+  assert(os.path.exists(tests_folder("conftest.py")))
